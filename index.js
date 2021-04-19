632ec9efc204fbe14b88369dcc534a61bd2c15f5
MicaRolle <micarolle_47@hotmail.com>
2021-04-19 18:57:20 -0300

    Add CORS

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const express = require("express");
 const app = express();
 const port = 4000;
+let user;
 
-function handleRegister(req, resp){
+app.use(express.json());
+
+app.use('*', function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Accept, Origin, Content-Type, access_token');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    next();
+});
+
+function handleRegister(req, res){
     user = req.body;
     res.send("Succesfull!");
     console.log(user);
 }
 
-function handleLogin(req, resp){
+function handleLogin(req, res){
     var userPassword = req.body.password;
     if ( user != null && user.password == userPassword){
         res.send("Succesfull");
